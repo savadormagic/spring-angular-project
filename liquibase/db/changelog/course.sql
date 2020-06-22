5cd28c3bc2ce3b00daa9702105a76f169e3c6182
Arkadiy <arksam@list.ru>
2020-06-22 22:04:17 +0500

    ДЗ 1. Liquibase, jooq

---
+++ liquibase/db/changelog/course.sql
@@ -0,0 +1,21 @@
+--liquibase formatted sql
+--changeset samoylov:20200621-01
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 select count(*) from (SELECT to_regclass('public.course') as res) as r where  res IS NOT NULL
+
+create  table  course  (
+    id          integer      not null
+            primary key,
+    idd         integer      not null,
+    name  varchar(100)  not null,
+    description   varchar(100)  not null,
+    teacher_idd integer not null,
+    max_count_student integer not null,
+    start_date  timestamp(0) not null,
+    end_date timestamp(0) not null,
+    create_date timestamp(0) not null,
+    delete_date timestamp(0),
+    status      varchar(10)
+);
+--rollback drop table course;
+--comment: Создание таблицы course
