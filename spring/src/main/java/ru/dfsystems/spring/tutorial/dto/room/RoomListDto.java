4527c5fd108b2b9b748ebe93b456ddd4952b357e
polovnikov <polovnikov@dfsystems.ru>
2020-06-27 13:52:48 +0500

    Lesson 4. CRUD для объекта ROOM через JOOQ

--- spring/src/main/java/ru/dfsystems/spring/tutorial/dto/room/RoomListDto.java
+++ spring/src/main/java/ru/dfsystems/spring/tutorial/dto/room/RoomListDto.java
@@ -1,12 +1,16 @@
 package ru.dfsystems.spring.tutorial.dto.room;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import ru.dfsystems.spring.tutorial.dto.BaseListDto;
 
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomListDto extends BaseListDto {
     private String number;
     private String block;
